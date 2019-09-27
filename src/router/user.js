0feb834a9b30764a11e54befde713e0014f6c2c1
devmobin <mobinheydari114@gmail.com>
2019-09-27 18:32:55 +0330

    logout user

--- src/router/user.js
+++ src/router/user.js
@@ -68,4 +68,28 @@ router.delete('/me', auth, async ({ user }, res) => {
   }
 })
 
+router.get('/logout', auth, async ({ token, user }, res) => {
+  try {
+    user.tokens = user.tokens.filter(t => t.token !== token)
+
+    await user.save()
+
+    res.status(200).send()
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+router.get('/logoutAll', auth, async ({ user }, res) => {
+  try {
+    user.tokens = []
+
+    await user.save()
+
+    res.status(200).send()
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
 module.exports = router
