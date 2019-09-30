a9c6b8306f0b1dbf41b54858ce8018a587d05e8e
devmobin <mobinheydari114@gmail.com>
2019-09-30 14:31:29 +0330

    transaction router setup

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const userRouter = require('./router/user')
+const transactionRouter = require('./router/transaction')
 
 const app = express()
 
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/user', userRouter)
+app.use('/transaction', transactionRouter)
 
 app.use((req, res) => {
   res.status(404).send()
